5bb0c0b908e796763a4db7d6bd59fe7f7fa5f538
Isaiah <lqlp0011@gmail.com>
2023-06-26 06:54:24 +0800

    Changes to tequila_sweet.mk

--- tequila_sweet.mk
+++ tequila_sweet.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := tequila_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
